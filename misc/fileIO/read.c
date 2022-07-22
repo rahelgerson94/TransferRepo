07e0925b2283907d6a0b01dd590298796f9cf830
rahelgerson94 <rahelgerson94@gmail.com>
2022-07-22 13:46:49 -0700

    db stments

--- misc/fileIO/read.c
+++ misc/fileIO/read.c
@@ -53,14 +53,15 @@ int count_num_lines(char* path, int buff_size){
 
 char** read_(char* path, int buff_size){
 int num_lines = count_num_lines(path, buff_size);    
-char** out = malloc(sizeof(char*)* num_lines+1);
+char** out = malloc(sizeof(char*)* (num_lines+1));
     for (int i = 0; i < num_lines+1; i++)
+        printf("out[%d]", i);  //db
         out[i] = NULL;
     FILE* in = fopen(path, "r");
     char curr_data[buff_size];
     int l = 0;
     int num_buffs = 1;
-    
+    int iter0 = 0; //db
     while(fgets(curr_data, buff_size, in) != NULL){
         if (out[l] == NULL){
             out[l] = calloc(sizeof(char), buff_size);
@@ -71,6 +72,7 @@ char** out = malloc(sizeof(char*)* num_lines+1);
             char temp[len];
             reset_char_arr(temp, 0, len);
             strcpy(temp, out[l]); //temp = out[l]
+            printf("\t%d. realloc( ) ", iter0);  //db
             out[l] = realloc(out[l], len);
             reset_char_arr(out[l], 0, len);
             strcpy(out[l], temp); //temp = out[l]
@@ -81,14 +83,14 @@ char** out = malloc(sizeof(char*)* num_lines+1);
                 curr_data[new_line_loc] = '\0';
                 
                 strncat(out[l], curr_data, buff_size*(num_buffs));
-                printf("%d. %s\n", l, out[l]);
+                //printf("%d. %s\n", l, out[l]);
                 //printf("%s\n", out[l]);
                 num_buffs = 1;
                 l++;
             }
             else{
                 strncat(out[l], curr_data, buff_size);
-                printf("%d. %s\n", l, out[l]);
+                //printf("%d. %s\n", l, out[l]);
                 num_buffs = num_buffs+1;
             }
         }
